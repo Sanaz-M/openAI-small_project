5a7bd653c2a6ece930e4b803cf34a3df594cd079
Sanaz M <sanazmohammaddi@gmail.com>
2023-08-14 17:39:40 +0100

    simple react app using OpenAi API key

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tweet, setTweet] = useState("");
+  const [sentiment, setSentiment] = useState(""); // "Negative" or "Positive"
 
+  async function callOpenAIAPI() {
+    console.log("Calling the OpenAI API");
+
+    // For 0-10
+    // What is the sentiment of this tweet with a value between 0 and 10 (10 being its very positive)? 
+
+    const APIBody = {
+      "model": "text-davinci-003",
+      "prompt": "What is the sentiment of this tweet? " + tweet,
+      "temperature": 0,
+      "max_tokens": 60,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
+    }
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + API_KEY
+        },
+        body: JSON.stringify(APIBody)
+      });
+
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data && data.choices && data.choices.length > 0) {
+        setSentiment(data.choices[0].text.trim());
+      } else {
+        console.error('Unexpected API response:', data);
+      }
+    } catch (error) {
+      console.error("Error while fetching sentiment from OpenAI API:", error);
+    }
+
+  }
+
+  console.log(tweet);
   return (
-    <>
+    <div className="App">
+      <h1>OpenAI Tweet Sentiment</h1>
+      <h3>The OpenAI API key was used along with its Tweet Classifier Example!</h3>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea
+          onChange={(e) => setTweet(e.target.value)}
+          placeholder='Paste your tweet here!'
+          cols={50}
+          rows={10}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={callOpenAIAPI}>Get The Tweet Sentiment From OpenAI API</button>
+        {sentiment !== "" ?
+          <h3>This Tweet Is: {sentiment}</h3>
+          :
+          null
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
